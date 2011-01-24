12779d5af4b3e56a751582df15c21022f3a6526f
L. Grondin <lucien@aptosidbox.(none)>
2011-01-24 07:06:51 +0100

    Using a string to store priv key

--- bc-sign.sh
+++ bc-sign.sh
@@ -4,7 +4,7 @@
 #
 # requires bc_key (https://github.com/dirtyfilthy/bc_key)
 
-. base58.sh
+. $HOME/bin/base58.sh
 
 error() { echo "${@:2}" 2>&1; exit $1; }
 
@@ -13,15 +13,15 @@ then error 1 "Usage: $0  bitcoinaddress [wallet]"
 elif
     addr="$1"  wallet="${2:-$HOME/.bitcoin/wallet.dat}"
     ! checkBitcoinAddress "$addr"
-then error 2 "Wrong format for bitcoin address"
+then error 2 "Invalid bitcoin address"
 elif ! which bc_key >/dev/null
 then error 3 "Please install bitcoin key extractor"
 elif ! which bc >/dev/null
 then error 4 "Please install bc (basic calculator)" 
 else
-    openssl dgst -sha256 -sign <(bc_key "$addr" "$wallet") -hex |
+    privkey="$(bc_key "$addr" "$wallet")"
+    openssl dgst -sha256 -sign <(echo "$privkey") -hex |
     sed 's/.* \s*//'
 
-    bc_key "$addr" "$wallet" |
-    openssl ec -pubout 2>/dev/null
+    openssl ec -pubout 2>&- <<<"$privkey"
 fi
