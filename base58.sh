840e1d6228d5dd72964dd3143254e37a6fae5cba
L. Grondin <lucien@aptosidbox>
2011-03-03 16:38:27 +0100

    Adding MIT license

--- base58.sh
+++ base58.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+# Copyright (C) 2011 by Lucien Grondin
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 # 
 # Requires bc, dc, openssl, xxd
 #
@@ -26,8 +45,8 @@ encodeBase58() {
 checksum() {
     xxd -p -r <<<"$1" |
     openssl dgst -sha256 -binary |
-    openssl dgst -sha256 -hex |
-    sed 's/^.* //' |
+    openssl dgst -sha256 -binary |
+    xxd -p -c 80 |
     head -c 8
 }
 
@@ -77,6 +96,6 @@ bitcoinHash() {
     xxd -p -r |
     openssl dgst -sha256 -binary |
     openssl dgst -sha256 -binary |
-    xxd -p |
+    xxd -p -80 |
     bigEndianHex2littleEndianHex
 }
