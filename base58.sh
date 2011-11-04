84052da464233820f25575f3e1eaeb66f450968b
Lucien Grondin <grondilu@blacky>
2011-11-04 13:24:10 +0100

    Full rewrite.  Added a function to eport to Wallet Impport Format.

--- base58.sh
+++ base58.sh
@@ -1,65 +1,42 @@
 #!/bin/bash
-# 
-# This is free and unencumbered software released into the public domain.
-# 
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-# 
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
+# Satoshi Nakamoto's Base58 encoding
 #
-#
-# Requires bc, dc, openssl, xxd
-#
+# requires dc, the unix desktop calculator (which should be included in the
+# 'bc' package)
 
-base58=({1..9} {A..H} {J..N} {P..Z} {a..k} {m..z})
-bitcoinregex="^[$(printf "%s" "${base58[@]}")]{34}$"
+declare -a base58=({1..9} {A..H} {J..N} {P..Z} {a..k} {m..z})
+unset dcr; for i in {0..57}; do dcr+="${i}s${base58[i]}"; done     # dc registers
 
 decodeBase58() {
-    local s=$1
-    for i in {0..57}
-    do s="${s//${base58[i]}/ $i}"
-    done
-    dc <<< "16o0d${s// /+58*}+f" 
+    dc -e "$dcr 16o0$(sed 's/./ 58*l&+/g' <<<$1)p" |
+    while read l; do echo -n "$l"; done
 }
 
 encodeBase58() {
-    # 58 = 0x3A
-    bc <<<"ibase=16; n=${1^^}; while(n>0) { n%3A ; n/=3A }" |
-    tac |
-    while read n
-    do echo -n ${base58[n]}
-    done
+    dc -e "16i ${1^^} [3A ~r d0<x]dsxx +f" |
+    while read n; do echo -n "${base58[n]}"; done
 }
 
 checksum() {
-    xxd -p -r <<<"$1" |
+    perl -we "print pack 'H*', '$1'" |
     openssl dgst -sha256 -binary |
     openssl dgst -sha256 -binary |
-    xxd -p -c 80 |
-    head -c 8
+    perl -we "print unpack 'H8', <>"
 }
 
 checkBitcoinAddress() {
-    if [[ "$1" =~ $bitcoinregex ]]
+    if [[ "$1" =~ ^[$(IFS= ; echo "${base58[*]}")]+$ ]]
     then
-        h=$(decodeBase58 "$1")
-        checksum "00${h::${#h}-8}" |
+	# a bitcoin address should be 25 bytes long:
+	# - 20 bytes for the hash160 (16/8 = 2, right?);
+	# -  1 byte  for the version number;
+	# -  4 bytes for the checksum.
+	# shorter addresses can only occur with standard 00 version number
+	# and should be zero padded
+	# Of course, in hex you must mutiply these numbers by 2
+	# to get the number of nybbles (hex digits)
+	h="$(printf "%50s" $(decodeBase58 "$1")| sed 's/ /0/g')"
+        checksum "${h::${#h}-8}" |
         grep -qi "^${h: -8}$"
     else return 2
     fi
@@ -68,39 +45,45 @@ checkBitcoinAddress() {
 hash160() {
     openssl dgst -sha256 -binary |
     openssl dgst -rmd160 -binary |
-    xxd -p -c 80
+    perl -we "print unpack 'H*', <>"
 }
 
-hash160ToAddress() {
-    printf "%34s\n" "$(encodeBase58 "00$1$(checksum "00$1")")" |
-    sed "y/ /1/"
+hashToAddress() {
+    local version="${2:-00}" size="${3:-160}"
+    local x="$(dc -e "${size}ss 16dio 2 ls^ ${version^^}* ${1^^}+p")"
+    printf "%34s\n" "$(encodeBase58 "$x$(checksum "$x")")" |
+    sed -r "s/ +/1/"
 }
 
 publicKeyToAddress() {
-    hash160ToAddress $(
-    openssl ec -pubin -pubout -outform DER |
+    hashToAddress "$(
+    openssl ec -pubin -pubout -outform DER 2>&- |
     tail -c 65 |
     hash160
-    )
+    )" "${1:-00}"
 }
 
-timestamp() {
-    hash160ToAddress "$(hash160)"
+privateKeyToWIF() {
+    hashToAddress "$(
+    openssl ec -text -noout 2>&- |
+    sed -n '3,5s/[: ]//gp' |
+    while read l; do echo -n "$l"; done
+    )" 80 256
 }
 
-bigEndianHex2littleEndianHex() {
-    local s=''
-    while read -n 2 char
-    do s=$char$s
+WIFtoDER() {
+    printf "%64s\n" "$(dc -e "16dio $(decodeBase58 $1) 100 4^ / 80 100 20^*-p")" |
+    sed 's/ /0/g'
+}
+
+vanityAddress() {
+    local addr priv
+    while ! grep -qi "${1:-1}" <<< "$addr"
+    do
+	priv="$(openssl ecparam -genkey -name secp256k1 2>&-)"
+	addr="$(openssl ec -pubout 2>&- <<<"$priv" | publicKeyToAddress)"
+	WIF="$(privateKeyToWIF <<<"$priv")"
     done
-    echo $s
-}
-
-bitcoinHash() {
-    bigEndianHex2littleEndianHex |
-    xxd -p -r |
-    openssl dgst -sha256 -binary |
-    openssl dgst -sha256 -binary |
-    xxd -p -80 |
-    bigEndianHex2littleEndianHex
+    echo "$addr, $WIF"
+    echo "$priv"
 }
