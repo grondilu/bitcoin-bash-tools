2aa3833ca1f451824343f1cc3ff45576eae3c998
Lucien Grondin <grondilu@outlook.fr>
2021-08-18 12:15:55 +0200

    declare some variables as readonly

--- bech32.sh
+++ bech32.sh
@@ -1,22 +1,17 @@
 # BECH32
 # see https://en.bitcoin.it/wiki/BIP_0173
-declare -a bech32=(
+readonly -a bech32=(
   q p z r y 9 x 8
   g f 2 t v d w 0
   s 3 j n 5 4 k h
   c e 6 m u a 7 l
 )
-declare -A bech32A
-for i in {0..31}
-do bech32A[${bech32[$i]}]=$i
-done
-
 # character class allowed for human readable part
 # hopefully this should match the ASCII range 33 .. 126
-declare HRP_CHAR_CLASS="[[:alnum:][:punct:]$+<=>^\`|~]"
+readonly HRP_CHAR_CLASS="[[:alnum:][:punct:]$+<=>^\`|~]"
 
 bech32_polymod() {
-  declare -ia GEN=(0x3b6a57b2 0x26508e6d 0x1ea119fa 0x3d4233dd 0x2a1462b3)
+  readonly -a GEN=(0x3b6a57b2 0x26508e6d 0x1ea119fa 0x3d4233dd 0x2a1462b3)
   declare -i  chk=1 b i v
   while read v
   do
@@ -59,6 +54,10 @@ bech32_encode() {
   }
 }
 bech32_decode() {
+  local -A bech32A
+  for i in {0..31}
+  do bech32A[${bech32[i]}]=$i
+  done
   echo -n "${1%1*}" |
   while read -n 1 c
   do LC_CTYPE=C printf '%d\n' "'$c"
@@ -75,8 +74,6 @@ bech32_verify_checksum() {
 bech32_verify() {
   local s="${1,,}"
   (( ${#s} > 90 )) && return 1
-  # hrp character set is only approximated here
-  # data character set is more rigorous
   [[ "${s,,}" =~ ^($HRP_CHAR_CLASS{1,83})1([02-9ac-hj-np-z]{6,})$ ]] || return 2
   bech32_verify_checksum "$s" || return 3
 }
