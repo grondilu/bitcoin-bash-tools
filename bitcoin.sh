aed8717079040fdd66327c33e80969c8918d56a1
Lucien Grondin <grondilu@outlook.fr>
2021-08-21 12:03:51 +0200

    minor tweaks

--- bitcoin.sh
+++ bitcoin.sh
@@ -37,20 +37,15 @@ fi
 
 . bech32.sh
 
-pack() {
-    echo -n "$1" |
-    xxd -r -p
-}
-unpack() {
-  xxd -p | tr -d '\n'
-}
+pack()   { echo -n "$1" | xxd -r -p; }
+unpack() { xxd -p | tr -d '\n'; }
 
 readonly -a base58=(
       1 2 3 4 5 6 7 8 9
     A B C D E F G H   J K L M N   P Q R S T U V W X Y Z
     a b c d e f g h i j k   m n o p q r s t u v w x y z
 )
-unset dcr; for i in {0..57}; do dcr+="${i}s${base58[i]}"; done
+unset dcr; for i in ${!base58[@]}; do dcr+="${i}s${base58[i]}"; done
 readonly secp256k1='
 I16i7sb0sa[[_1*lm1-*lm%q]Std0>tlm%Lts#]s%[Smddl%x-lm/rl%xLms#]s~
 483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
@@ -66,6 +61,22 @@ s#Lds#Lxs#Lms#]sA[rs.0r[rl.lAxr]SP[q]sQ[d0!<Qd2%1=P2/l.lDxs.lLx]
 dSLxs#LPs#LQs#]sM[lpd1+4/r|]sR
 ';
 
+point() {
+  [[ "$1" =~ ^[[:xdigit:]]+$ ]] && dc -e "
+    I16i7sb0sa[[_1*lm1-*lm%q]Std0>tlm%Lts#]s%[Smddl%x-lm/rl%xLms#]s~
+    483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+    79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
+    2 100^d14551231950B75FC4402DA1732FC9BEBF-so1000003D1-ddspsm*+sGi
+    [_1*l%x]s_[+l%x]s+[*l%x]s*[-l%x]s-[l%xsclmsd1su0sv0sr1st[q]SQ[lc
+    0=Qldlcl~xlcsdscsqlrlqlu*-ltlqlv*-lulvstsrsvsulXx]dSXxLXs#LQs#lr
+    l%x]sI[lpSm[+q]S0d0=0lpl~xsydsxd*3*lal+x2ly*lIx*l%xdsld*2lx*l-xd
+    lxrl-xlll*xlyl-xrlp*+Lms#L0s#]sD[lpSm[+q]S0[2;AlDxq]Sdd0=0rd0=0d
+    2:Alp~1:A0:Ad2:Blp~1:B0:B2;A2;B=d[0q]Sx2;A0;B1;Bl_xrlm*+=x0;A0;B
+    l-xlIxdsi1;A1;Bl-xl*xdsld*0;Al-x0;Bl-xd0;Arl-xlll*x1;Al-xrlp*+L0
+    s#Lds#Lxs#Lms#]sA[rs.0r[rl.lAxr]SP[q]sQ[d0!<Qd2%1=P2/l.lDxs.lLx]
+    dSLxs#LPs#LQs#]sM[lpd1+4/r|]sR lG I16i${1^^}ri lMx 16olm~ n[ ]nn
+    "
+}
 decodeBase58() {
   echo -n "$1" | sed -e's/^\(1*\).*/\1/' -e's/1/00/g' | tr -d '\n'
   echo "$1" |
