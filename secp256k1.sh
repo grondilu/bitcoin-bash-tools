d4370bfe1f60c367c6085cee74aa1ec1cdc153c1
Lucien Grondin <grondilu@outlook.fr>
2021-08-27 00:04:20 +0200

    use E macro

--- secp256k1.sh
+++ secp256k1.sh
@@ -3,13 +3,13 @@ then
   1>&2 echo "could not find dc script file"
 fi
 
-point()
+secp256k1()
   if [[ "$1" =~ ^-h|--help$ ]]
   then
     cat <<-EOF
 	Usage:
-	  point [exponent]
-	  point -h|--help
+	  secp256k1 [exponent]
+	  secp256k1 -h|--help
 	
 	Display the compressed coordinates of a point on the secp256k1 curve
 	given an exponent.
@@ -21,79 +21,27 @@ point()
 	rand -hex 32'
 	EOF
   elif (( $# == 0 ))
-  then $FUNCNAME "0x$(openssl rand -hex 32)"
+  then
+    {
+      echo 16doi0
+      sed 's/.*/&dlYxr2 2 8^^%lm*+lAx/'
+      echo lEx
+    } | dc -f secp256k1.dc -
   elif [[ "$1" =~ ^[[:digit:]]+$ ]]
   then $FUNCNAME "0x$(dc -e "$1 16on")"
   elif [[ "$1" =~ ^0x([[:xdigit:]]+)$ ]]
-  then
-    dc -f secp256k1.dc -e "16doi${BASH_REMATCH[1]^^}dlGrlMxlm~rf" |
-    compressPoint
+  then dc -f secp256k1.dc -e "16doi${BASH_REMATCH[1]^^}dlGrlMxlEx"
+  elif [[ "$1" = '-u' ]]
+  then shift
+    dc -f secp256k1.dc -e "4 2 512^*16doi$1dlYxr2 2 8^^%2 2 8^^*++P" |
+    xxd -p -u -c 65
   else
     1>&2 echo wrong argument format
     return 1
   fi
 
-uncompressPoint() {
-  echo -n "04"
-  dc -f secp256k1.dc -e "16doi$1dlYxr2 2 8^^%f" |
-  while read
-  do ser256 "$REPLY"
-  done |
-  xxd -p -u -c 128
-}
-compressPoint() {
-  local x y
-  read x
-  read y
-  x="$(ser256 "$x" |xxd -u -p -c64)"
-  y="$(ser256 "$y" |xxd -u -p -c64)"
-  if [[ "$y" =~ [02468ACE]$ ]]
-  then echo "02$x"
-  else echo "03$x"
-  fi
-}
-add() {
-  if [[ "$1" =~ ^-h|--help$ ]]
-  then
-    cat <<-END
-	Usage: add [-h|--help]
-	
-	Read compressed secp256k1 point coordinates on stdin and outputs the
-	compressed coordinates of the sum of the corresponding points.
-	END
-  else
-    {
-      echo 16doi0
-      sed 's/.*/&dlYxr2 2 8^^%lm*+lAx/'
-      echo 'lm~rf'
-    } |
-    dc -f secp256k1.dc - |
-    compressPoint
-  fi
-}
-ser32()
-  if
-    local -i i=$1
-    ((i >= 0 && i < 1<<32)) 
-  then printf "%08x" $i |xxd -p -r
-  else
-    1>&2 echo index out of range
-    return 1
-  fi
-
-ser256()
-  if [[ "$1" =~ ^(0x)?([[:xdigit:]]+)$ ]]
-  then
-    dc -e "16i 2 100^ ${BASH_REMATCH[2]^^}+ P" |
-    tail -c 32
-  fi
-
 parse256() { xxd -u -p -c32; }
-serP() {
-  jq -r .point |
-  xxd -r -p -c66
-}
 parseP() {
-  point "$(xxd -u -p -c33)"
+  secp256k1 "$(xxd -u -p -c33)"
 }
 
