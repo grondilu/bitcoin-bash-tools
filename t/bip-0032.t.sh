d82aaecbd1817ab39adbc6e00c1683f1cfa466e5
Lucien Grondin <grondilu@outlook.fr>
2021-08-27 20:04:43 +0200

    add bip 32 test files

---
+++ t/bip-0032.t.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+. bip-0032.sh
+
+echo 1..1
+
+declare -i t=0
+jq -r '.[]|[.seed,.m."ext prv"]|join(" ")' t/bip-0032.t.json |
+{
+  while read seed xprv
+  do 
+    ((t++))
+    declare result="$(echo -n "$seed" |xxd -p -r |bip32 M)"
+    if [[ "$result" = "$xprv" ]]
+    then echo "ok $t - $seed → ${xprv::10}...${xprv: -10}"
+    else echo "not ok $t - seed $seed, $xpriv expected, got $result"
+    fi
+  done
+}
