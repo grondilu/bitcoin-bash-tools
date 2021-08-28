4744b4dd68b906ed78c8825253893785decf4243
Lucien Grondin <grondilu@outlook.fr>
2021-08-28 14:37:42 +0200

    fix case

--- t/bip-0032.t.sh
+++ t/bip-0032.t.sh
@@ -10,7 +10,7 @@ jq -r '.[]|[.seed,.m."ext prv"]|join(" ")' t/bip-0032.t.json |
   while read seed xprv
   do 
     ((t++))
-    declare result="$(echo -n "$seed" |xxd -p -r |bip32 M)"
+    declare result="$(echo -n "$seed" |xxd -p -r |bip32 m)"
     if [[ "$result" = "$xprv" ]]
     then echo "ok $t - $seed → ${xprv::10}...${xprv: -10}"
     else echo "not ok $t - seed $seed, $xpriv expected, got $result"
