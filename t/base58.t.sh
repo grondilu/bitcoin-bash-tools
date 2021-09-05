02f9c7332e1003662b47afbc8e45767aad4a2863
Lucien Grondin <grondilu@outlook.fr>
2021-09-05 13:01:39 +0200

    modify test file to test bx.sh

--- t/base58.t.sh
+++ t/base58.t.sh
@@ -1,6 +1,6 @@
 json="t/base58_encode_decode.json"
 
-. base58.sh
+. bx.sh
 
 echo 1..$(jq length < $json)
 declare -i t=0
@@ -8,7 +8,7 @@ jq -r ".[]|\"\(.[0]) \(.[1])\"" < $json |
 while read a b
 do
   ((t++))
-  declare c=$(xxd -p -r <<<$a |encodeBase58)
+  declare c="$(bx base58-encode "$a")"
   if [[ "$c" = "$b" ]]
   then echo "ok $t - $a → $b"
   else echo "not ok $t - $a → $c instead of $b"
