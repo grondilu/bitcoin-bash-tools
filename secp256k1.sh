448ad97c56430f7ea042cf4036e94787f034d66f
Lucien Grondin <grondilu@outlook.fr>
2021-09-10 21:02:59 +0200

    properly parse options

--- secp256k1.sh
+++ secp256k1.sh
@@ -12,33 +12,20 @@ secp256k1()
       echo lEx
     } | dc -f secp256k1.dc -
   elif
-    local OPTIND o
-    getopts u o
+    local OPTIND OPTARG o
+    getopts hu: o
   then
     shift $((OPTIND - 1))
-    dc -f secp256k1.dc -e "4 2 512^*16doi$1dlYxr2 2 8^^%2 2 8^^*++P" |
-    xxd -p -u -c 65
-  elif (( $# > 1 ))
-  then
-    local i
-    {
-      echo 0
-      for i in "$@"
-      do
-        if [[ "$i" =~ ^[[:digit:]]+$ ]]
-        then echo "5d+i$i+"
-	elif [[ "$i" =~ ^0x([[:xdigit:]]+)$ ]]
-        then echo "8d+i${BASH_REMATCH[1]^^}+"
+    case "$o" in
+      u)
+        if [[ "$OPTARG" =~ ^0[23][[:xdigit:]{2}{32}$ ]]
+        then
+	  dc -f secp256k1.dc -e "4 2 512^*16doi${OPTARG^^}dlYxr2 2 8^^%2 2 8^^*++P" |
+	  xxd -p -u -c 65
+        else return 7
         fi
-      done
-      echo 'ln%[0x]P8d+op'
-    } | dc -f secp256k1.dc -
-  elif [[ "$1" =~ ^[[:digit:]]+$ ]]
-  then $FUNCNAME "0x$(dc -e "$1 16on")"
-  elif [[ "$1" =~ ^(0x)?([[:xdigit:]]+)$ ]]
-  then dc -f secp256k1.dc -e "16doilG${BASH_REMATCH[2]^^}lMxlEx"
-  else
-    cat <<-EOF
+        ;;
+      h) cat <<-EOF
 	Usage:
 	  secp256k1
 	  secp256k1 exponent
@@ -59,6 +46,28 @@ secp256k1()
 	An exponent is a natural integer in either decimal or hexadecimal format
 	(with the 0x prefix for hexadecimal).
 	EOF
+        ;;
+    esac
+  elif (( $# > 1 ))
+  then
+    local i
+    {
+      echo 0
+      for i in "$@"
+      do
+        if [[ "$i" =~ ^[[:digit:]]+$ ]]
+        then echo "5d+i$i+"
+	elif [[ "$i" =~ ^0x([[:xdigit:]]+)$ ]]
+        then echo "8d+i${BASH_REMATCH[1]^^}+"
+        fi
+      done
+      echo 'ln%[0x]P8d+op'
+    } | dc -f secp256k1.dc -
+  elif [[ "$1" =~ ^[[:digit:]]+$ ]]
+  then $FUNCNAME "0x$(dc -e "$1 16on")"
+  elif [[ "$1" =~ ^(0x)?([[:xdigit:]]+)$ ]]
+  then dc -f secp256k1.dc -e "16doilG${BASH_REMATCH[2]^^}lMxlEx"
+  else return 1
   fi
 
 parse256() { xxd -u -p -c32; }
