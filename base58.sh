3c5b45c6f0cc52bd3de4e3ecbc5997dcb2a10bcb
Lucien Grondin <grondilu@outlook.fr>
2021-09-10 10:49:28 +0200

    add base58 function

--- base58.sh
+++ base58.sh
@@ -1,9 +1,76 @@
-declare -a base58=(
+declare -a base58_chars_str="123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+declare -a base58_chars=(
     1 2 3 4 5 6 7 8 9
   A B C D E F G H   J K L M N   P Q R S T U V W X Y Z
   a b c d e f g h i j k   m n o p q r s t u v w x y z
 )
-unset dcr; for i in ${!base58[@]}; do dcr+="${i}s${base58[i]}"; done
+unset dcr; for i in ${!base58_chars[@]}; do dcr+="${i}s${base58_chars[i]}"; done
+
+base58()
+  if
+    # echo BASE58_OPERATION=$BASE58_OPERATION $FUNCNAME "$@" >&2
+    (($# == 0))
+  then $FUNCNAME "$(xxd -p |while read; do echo -n "$REPLY"; done)"
+  elif
+    local OPTIND OPTARG o
+    getopts hxdvc o
+  then
+    shift $((OPTIND - 1))
+    case $o in
+      d) BASE58_OPERATION=decode-to-binary $FUNCNAME "$@";;
+      v) BASE58_OPERATION=verify-checksum  $FUNCNAME "$@";;
+      x) $FUNCNAME -d "$@" |
+         xxd -p |
+         while read; do echo -n $REPLY; done
+         echo
+        ;;
+      c) BASE58_USE_CHECKSUM=yes $FUNCNAME "$@";;
+      h)
+        cat <<-END_USAGE
+	$FUNCNAME [options] [hex or base58 string]
+	
+	options are:
+	  -h:	show this help
+	  -d:	decode from base58 to binary
+	  -x:	decode from base58 to hexadecimal
+	  -c:	append checksum
+          -v:	verify checksum
+	END_USAGE
+        return
+        ;;
+    esac
+  elif [[ "$BASE58_OPERATION" = decode-to-binary ]]
+  then
+    if [[ "$1" =~ ^1+ ]]
+    then printf "\x00"; $FUNCNAME "${1:1}"
+    elif [[ "$1" =~ ^[$base58_chars_str]+$ ]]
+    then sed -e "i$dcr 0" -e 's/./ 58*l&+/g' -e "aP" <<<"$1" | dc
+    else return 2
+    fi
+  elif [[ "$BASE58_OPERATION" = verify-checksum ]]
+  then
+    BASE58_OPERATION= $FUNCNAME -d "$1" |
+    head -c -4 |
+    BASE58_OPERATION= $FUNCNAME -c |
+    grep -q "^$1$"
+  elif [[ "$BASE58_USE_CHECKSUM" = yes ]]
+  then
+    {
+       xxd -p -r <<<"$1"
+       xxd -p -r <<<"$1" |
+       openssl dgst -sha256 -binary |
+       openssl dgst -sha256 -binary |
+       head -c 4
+    } | BASE58_USE_CHECKSUM= $FUNCNAME 
+  elif [[ "$1" =~ ^00 ]]
+  then echo -n 1; $FUNCNAME "${1:2}"
+  elif [[ "$1" =~ ^[[:xdigit:]]{2}+$ ]]
+  then sed -e 'i16i0' -e 's/../100*&+/g' -e 'a[3A~rd0<x]dsxx+f' <<<"${1^^}" |
+    dc |
+    while read; do echo -n ${base58_chars[REPLY]}; done
+    echo
+  else return 9
+  fi
 
 decodeBase58() {
   if [[ "$1" =~ ^1+ ]]
@@ -36,11 +103,12 @@ encodeBase58() {
 	done
 	echo '[3A~rd0<x]dsxx+f'  
       } | dc |
-      while read -r n; do echo -n "${base58[n]}"; done
+      while read -r n; do echo -n "${base58_chars[n]}"; done
     fi
     echo
   }
 }
+
 checksum() {
   openssl dgst -sha256 -binary |
   openssl dgst -sha256 -binary |
