46de426237273a2a66c7f30176969f3d723317c5
Lucien Grondin <grondilu@outlook.fr>
2021-09-15 11:43:39 +0200

    add comments about error cases

--- bech32.sh
+++ bech32.sh
@@ -1,93 +1,155 @@
-# BECH32
-# see https://en.bitcoin.it/wiki/BIP_0173
-if ! test -v bech32
-then readonly -a bech32=(
-  q p z r y 9 x 8
-  g f 2 t v d w 0
-  s 3 j n 5 4 k h
-  c e 6 m u a 7 l
-)
-fi
-declare -A bech32A
-for i in {0..31}
-do bech32A[${bech32[i]}]=$i
-done
-# character class allowed for human readable part
-# hopefully this should match the ASCII range 33 .. 126
-if ! test -v HRP_CHAR_CLASS
-then readonly HRP_CHAR_CLASS="[[:alnum:][:punct:]$+<=>^\`|~]"
-fi
+#!/usr/env/bin bash
 
-bech32_polymod() {
-  readonly -a GEN=(0x3b6a57b2 0x26508e6d 0x1ea119fa 0x3d4233dd 0x2a1462b3)
-  local    -i chk=1 b i v
-  while read v
+# Translated from javascript.
+#
+# Original code link:
+# https://github.com/sipa/bech32/blob/master/ref/javascript/bech32.js
+
+# Copyright and permission notice in original file :
+# 
+# // Copyright (c) 2017, 2021 Pieter Wuille
+# //
+# // Permission is hereby granted, free of charge, to any person obtaining a copy
+# // of this software and associated documentation files (the "Software"), to deal
+# // in the Software without restriction, including without limitation the rights
+# // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# // copies of the Software, and to permit persons to whom the Software is
+# // furnished to do so, subject to the following conditions:
+# //
+# // The above copyright notice and this permission notice shall be included in
+# // all copies or substantial portions of the Software.
+# //
+# // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# // THE SOFTWARE.
+
+declare BECH32_CHARSET="qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+declare -A BECH32_CHARSET_REVERSE
+for i in {0..31}
+do BECH32_CHARSET_REVERSE[${BECH32_CHARSET:$i:1}]=$((i))
+done
+
+
+BECH32_CONST=1
+
+polymod() {
+  local -ai generator=(0x3b6a57b2 0x26508e6d 0x1ea119fa 0x3d4233dd 0x2a1462b3)
+  local -i chk=1 value
+  for value
   do
-    ((
-      b=chk >> 25,
-      chk=(chk & 0x1ffffff) << 5^v
-    ))
-    for i in {0..4}
-    do ((chk^= (b >> i) & 1 ? GEN[i] : 0))
+    local -i top i
+    
+    ((top = chk >> 25, chk = (chk & 0x1ffffff) << 5 ^ value))
+    
+    for i in 0 1 2 3 4
+    do (( ((top >> i) & 1) && (chk^=${generator[i]}) ))
     done
   done
   echo $chk
 }
-bech32_hrp_expand() {
-  mapfile -t values
-  for x in ${values[@]}
-  do echo $(( x >> 5 ))
+
+hrpExpand() {
+  local -i p ord
+  for ((p=0; p < ${#1}; ++p))
+  do printf -v ord "%d" "'${1:$p:1}"
+    echo $(( ord >> 5 ))
   done
   echo 0
-  for x in ${values[@]}
-  do echo $(( x & 31 ))
+  for ((p=0; p < ${#1}; ++p))
+  do printf -v ord "%d" "'${1:$p:1}"
+    echo $(( ord & 31 ))
   done
 }
-bech32_decode() {
-  echo -n "${1%1*}" |
-  while read -n 1 c
-  do LC_CTYPE=C printf '%d\n' "'$c"
-  done |
-  bech32_hrp_expand
-  echo -n "${1##*1}" |
-  while read -n 1 c
-  do echo ${bech32A[$c]}
-  done
-}
-bech32_verify_checksum() ((
-  $(bech32_decode "${1,,}" | bech32_polymod) == 1
-))
-bech32_verify() {
-  (( ${#1} <= 90 )) || return 1
-  [[ "${1,,}" =~ ^($HRP_CHAR_CLASS{1,83})1([02-9ac-hj-np-z]{6,})$ ]] || return 2
-  bech32_verify_checksum "${1,,}" || return 3
-}
-bech32_create_checksum() {
-  local hrp="${1%1*}" data="${1##*1}"
-  {
-    echo -n "$hrp" |
-    while read -n 1
-    do LC_TYPE=C printf '%d\n' "'$REPLY"
-    done |
-    bech32_hrp_expand
 
-    echo -n "${1##*1}" |
-    while read -n 1 c
-    do echo ${bech32A[$c]}
-    done
-    
-    for i in {1..6}; do echo 0; done
-  } |
-  bech32_polymod |
-  {
-    read
-    local -i polymod=$(($REPLY ^ 1))
-    local -a checksum
-    for i in {0..5}
-    do checksum[i]=$(( (polymod >> 5 * (5 - i)) & 31 ))
-    done
-    for c in ${checksum[@]}
-    do echo -n ${bech32[$c]}
-    done
-  }
+verifyChecksum() {
+  local hrp="$1"
+  shift
+  local -i pmod="$(polymod $(hrpExpand "$hrp") "$@")"
+  (( pmod == $BECH32_CONST ))
 }
+
+createChecksum() {
+  local hrp="$1"
+  shift
+  local -i p mod=$(($(polymod $(hrpExpand "$hrp") "$@" 0 0 0 0 0 0) ^ $BECH32_CONST))
+  for p in 0 1 2 3 4 5
+  do echo $(( (mod >> 5 * (5 - p)) & 31 ))
+  done
+}
+
+bech32_encode()
+  if
+    local OPTIND o
+    getopts m o
+  then
+    shift $((OPTIND - 1))
+    BECH32_CONST=0x2bc830a3 $FUNCNAME "$@"
+  else
+    local hrp=$1 i
+    shift
+    echo -n "${hrp}1"
+    {
+      for i; do echo $i; done
+      createChecksum "$hrp" "$@"
+    } |
+    while read; do echo -n ${BECH32_CHARSET:$REPLY:1}; done
+    echo
+  fi
+
+bech32_decode()
+  if
+    local OPTIND o
+    getopts m o
+  then
+    shift $((OPTIND - 1))
+    BECH32_CONST=0x2bc830a3 $FUNCNAME "$@"
+  else
+    local bechString=$1
+    local -i p has_lower=0 has_upper=0 ord
+
+    for ((p=0;p<${#bechString};++p))
+    do
+      printf -v ord "%d" "'${bechString:$p:1}"
+      if   ((ord <  33 || ord >  126))
+      then return 1
+      elif ((ord >= 97 && ord <= 122))
+      then has_lower=1
+      elif ((ord >= 65 && ord <= 90))
+      then has_upper=1
+      fi
+    done
+    if ((has_upper && has_lower))
+    then return 2 # mixed case
+    elif bechString="${bechString,,}"
+      ((${#bechString} > 90))
+    then return 3 # string is too long
+    elif [[ ! "$bechString" =~ 1 ]]
+    then return 4 # no separator
+    elif
+      local hrp="${bechString%1*}"
+      test -z $hrp
+    then return 5 # no human readable part
+    elif local data="${bechString##*1}"
+      ((${#data} < 6))
+    then return 6 # data is too short
+    else
+      for ((p=0;p<${#data};++p))
+      do echo "${BECH32_CHARSET_REVERSE[${data:$p:1}]}"
+      done |
+      {
+	mapfile -t
+	if verifyChecksum "$hrp" "${MAPFILE[@]}"
+	then
+	  echo $hrp
+	  for p in "${MAPFILE[@]::${#MAPFILE[@]}-6}"
+	  do echo $p
+	  done
+	else return 8
+	fi
+      }
+    fi
+  fi
