447395ffdffd7cbfc2061454a178933c7156e780
Lucien Grondin <grondilu@outlook.fr>
2021-09-21 17:25:38 +0200

    make a unique pbkdf2 function

--- bip-0039.sh
+++ bip-0039.sh
@@ -1,104 +1,106 @@
 
-function pbkdf2_python {
-  local command_str
-  printf -v command_str 'import hashlib; print(hashlib.pbkdf2_hmac("%s","%s".encode("utf-8"), "%s".encode("utf-8"), %d).hex())' "$@"
-  python -c "$command_str"
-}
-
 ceil() { echo $(( ($1 + $2 + 1)/$2 )); }
 
-function pbkdf2_bash {
-  # Translated from https://github.com/bitpay/bitcore/blob/master/packages/bitcore-mnemonic/lib/pbkdf2.js
-  # /**
-  # * PDKBF2
-  # * Credit to: https://github.com/stayradiated/pbkdf2-sha512
-  # * Copyright (c) 2014, JP Richardson Copyright (c) 2010-2011 Intalio Pte, All Rights Reserved
-  # */
-  local -ai key salt dk u t block1
-  local -i iterations=$3 dkLen=$4 hLen=64 i j k destPos len
+function pbkdf2() {
+  case "$PBKDF2_METHOD" in
+    python)
+      local command_str
+      printf -v command_str 'import hashlib; print(hashlib.pbkdf2_hmac("%s","%s".encode("utf-8"), "%s".encode("utf-8"), %d).hex())' "$@"
+      python -c "$command_str"
+      ;;
+    *)
+      # Translated from https://github.com/bitpay/bitcore/blob/master/packages/bitcore-mnemonic/lib/pbkdf2.js
+      # /**
+      # * PDKBF2
+      # * Credit to: https://github.com/stayradiated/pbkdf2-sha512
+      # * Copyright (c) 2014, JP Richardson Copyright (c) 2010-2011 Intalio Pte, All Rights Reserved
+      # */
+      local hash_name="$1" key_str="$2" salt_str="$3"
+      local -ai key salt u t block1
+      local -i hLen="$(openssl dgst -"$hash_name" -binary <<<"foo" |wc -c)"
+      local -i iterations=$4 dkLen=${5:-hLen} i j k destPos len
+      
+      local -i l=dkLen/hLen
+      local -i r=$((dkLen-(l-1)*hLen))
 
-  local -i l=dkLen/hLen
-  local -i r=$((dkLen-(l-1)*hLen))
-  
-  local c
-  for ((i=0; i<${#1}; i++))
-  do
-    printf -v c "%d" "'${1:i:1}"
-    key+=($c)
-  done
-
-  for ((i=0; i<${#2}; i++))
-  do
-    printf -v c "%d" "'${2:i:1}"
-    salt+=($c)
-  done
-
-  for ((i=0; i<dKlen; i++)); do dk+=(0); done
-  for ((i=0; i< hLen; i++)); do u+=(0); t+=(0); done
-
-  for c in ${salt[@]}; do block1+=($c); done
-  for i in {1..4}; do block1+=(0); done
-
-  for ((i=1;i<=l;i++))
-  do
-    block1[${#salt[@]}+0]=$((i >> 24 & 0xff))
-    block1[${#salt[@]}+1]=$((i >> 16 & 0xff))
-    block1[${#salt[@]}+2]=$((i >>  8 & 0xff))
-    block1[${#salt[@]}+3]=$((i >>  0 & 0xff))
-    
-    u=($(
-      for c in ${block1[@]}
-      do printf '%02x\n' $c
-      done |
-      while read
-      do printf %b "\x$REPLY"
-      done |
-      openssl dgst -sha512 -hmac "$1" -binary |
-      xxd -p -c 1 |
-      while read
-      do echo $((0x$REPLY))
+      local c
+      for ((i=0; i<${#key_str}; i++))
+      do
+	printf -v c "%d" "'${key_str:i:1}"
+	key+=($c)
       done
-    ))
-    printf "PBKFD2 iteration %10d/%d" 1 $iterations >&2
-    t=(${u[@]})
-    for ((j=1; j<iterations; j++))
-    do
-      printf "\rPBKFD2 iteration %10d/%d" $((j+1)) $iterations >&2
-      u=($(
-	for c in ${u[@]}
-	do printf '%02x\n' $c
-	done |
-	while read
-	do printf %b "\x$REPLY"
-	done |
-	openssl dgst -sha512 -hmac "$1" -binary |
-	xxd -p -c 1 |
-	while read
-	do echo $((0x$REPLY))
+
+      for ((i=0; i<${#salt_str}; i++))
+      do
+	printf -v c "%d" "'${salt_str:i:1}"
+	salt+=($c)
+      done
+
+      for ((i=0; i<dKlen; i++)); do dk+=(0); done
+      for ((i=0; i< hLen; i++)); do u+=(0); t+=(0); done
+
+      for c in ${salt[@]}; do block1+=($c); done
+      for i in {1..4}; do block1+=(0); done
+
+      for ((i=1;i<=l;i++))
+      do
+	block1[${#salt[@]}+0]=$((i >> 24 & 0xff))
+	block1[${#salt[@]}+1]=$((i >> 16 & 0xff))
+	block1[${#salt[@]}+2]=$((i >>  8 & 0xff))
+	block1[${#salt[@]}+3]=$((i >>  0 & 0xff))
+	
+	u=($(
+	  for c in ${block1[@]}
+	  do printf '%02x\n' $c
+	  done |
+	  while read
+	  do printf %b "\x$REPLY"
+	  done |
+	  openssl dgst -sha512 -hmac "$key_str" -binary |
+	  xxd -p -c 1 |
+	  while read
+	  do echo $((0x$REPLY))
+	  done
+	))
+	printf "PBKFD2 iteration %10d/%d" 1 $iterations >&2
+	t=(${u[@]})
+	for ((j=1; j<iterations; j++))
+	do
+	  printf "\rPBKFD2 iteration %10d/%d" $((j+1)) $iterations >&2
+	  u=($(
+	    for c in ${u[@]}
+	    do printf '%02x\n' $c
+	    done |
+	    while read
+	    do printf %b "\x$REPLY"
+	    done |
+	    openssl dgst -sha512 -hmac "$key_str" -binary |
+	    xxd -p -c 1 |
+	    while read
+	    do echo $((0x$REPLY))
+	    done
+	  ))
+	  for ((k=0; k<hLen; k++))
+	  do t[k]=$((t[k]^u[k]))
+	  done
 	done
-      ))
-      for ((k=0; k<hLen; k++))
-      do t[k]=$((t[k]^u[k]))
+	echo >&2
+	
+	destPos=$(( (i-1)*hLen ))
+	if ((i == l))
+	then len=r
+	else len=hLen
+	fi
+	for ((k=0; k<len; k++))
+	do dk[destPos+k]=${t[k]}
+	done
+	
       done
-    done
-    echo >&2
-    
-    destPos=$(( (i-1)*hLen ))
-    if ((i == l))
-    then len=r
-    else len=hLen
-    fi
-    for ((k=0; k<len; k++))
-    do dk[destPos+k]=${t[k]}
-    done
-    
-  done
-  printf "%02x" ${dk[@]}
-  echo
+      printf "%02x" ${dk[@]}
+      echo
+    ;;
+  esac
 }
-
-pbkdf2_bash foo bar 2048 64
-pbkdf2_python sha512 "foo" "bar" 2048
 
 function bip39() {
   if [ ! -L wordlist.txt ]
@@ -174,7 +176,7 @@ function bip39() {
     fi
     local passphrase
     read -p 'passphrase: ' passphrase
-    pbkdf2_python sha512 "$*" "mnemonic$passphrase" 2048
+    pbkdf2 sha512 "$*" "mnemonic$passphrase" 2048
   elif (($# == 0))
   then $FUNCNAME 160
   else
