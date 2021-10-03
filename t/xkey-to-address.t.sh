c066ecd18b1827fce3115bdc91fd5132f5f6605f
Lucien Grondin <grondilu@outlook.fr>
2021-10-03 20:30:50 +0200

    factorize code

--- t/xkey-to-address.t.sh
+++ t/xkey-to-address.t.sh
@@ -9,118 +9,85 @@
 
 echo "1..60"
 
-seed=b5f14640decb6bc5d1b52cba34f75a4643c3cd0096e1ec7adddca66c22e6b15e18118295fafe3766a38122f80fd3894e0a3ebbab6bdd7626dc1aefeb9c59ac52
-declare -a bip44=(
-  1LBfp6GkgPrCV9y1jTWRFA9b4GyjcZh3Ww
-  1Pm5nEaXQ7QD33ToheE1njxVguDBuNfCgN
-  1PdHpP1XY7YVmc3Hzqvr4J96htps77kApV
-  1HhK7y3az5PKxeQkhmu9csbc73Th8uh5Ac
-  1MJwwjmHvJGqHKPXAcSs641gPJi3CeigyL
-  1EkYX36wvfFAPxmsfeMy2DwRHUMEU9axxq
-  14aXeP7kUmmWN6bZqetKAwKjVoALnrad3p
-  1Axr9h8atojNHq3S1Mc6ykbkVmd46N5cPn
-  1PrLz8yhrpUH7xQdDTtTGXfYp1JG7aXaVo
-  1MjhhR9TmAUFYwp5AHeNPs6vm4Uedtc8g2
-  1P93tio4J9K5sDTyVWXv2K7aMsL6y8gQh2
-  18gv6wnRqxWWbvzeBwS7wRNTckdxVfreV2
-  1N39YpXqMs1ffUijswj7aAi738eeuv3e6o
-  1GtfrdKc95VNeh56vWDM4aPcbiKwYgYrVa
-  1MdL2rCa2mgsAZP5bovXRuEWq47ERXJ592
-  1DoxPu2RkUR4msiGZ51NPWHvpZp5zwvLgo
-  16YZCM6pAWdcwUPpYcKGzXoX5Lzva8BHot
-  1FDHU4ZN5i5Um3c2jSRoW1f1xF1HSxEt2V
-  1LgctGK9eo4tz7Cyw8SF4tGvvSLtj6sqrM
-  1Mt7RZoc4mKW3vsyFxyY5EAQSGDtRY13gc
-)
-
-declare -a bip49=(
-  3K8xDAJAiMLJ7DG9PKzQfT97sZmDUV1Eyn
-  3KQASQ2jgT9frWZWGeF6owtrve5XgK2V19
-  377oZaCuiUjkeDtHRggZjsAgwtNKECYDw8
-  3LezkRKYr2BPdRrPXLrh7YUDCQXZ9VpEYG
-  3DwgXNHms3XV1FZH2DRE8CXqJDFKjZPMR4
-  34g5kxR92Qs7hBt2qYyFTMw95qca5xHaT6
-  3BLkJUMwAx2VqBRNrSAhovyyhqcsFPdGkc
-  3Ju2iFoeNU1ZNAChsTStXeaymnGK1stvUc
-  3PuX9caB7LFf78e92KuJX1YjrGQSWr2Hhg
-  3DoHF5cZSF4g2JdxZGimrCrcy9KnoL5sfJ
-  3FxN4kX9sXFegF8BxWvXVmh3mgUXyu279n
-  3DoQu71wudhkvE3JMJv64pgLCHoUVry9X6
-  3357aJgZCHjBkHXFfxeLDyNHqUCzFHFrvS
-  35ysqAPaXZezdUz2wwcvQpE76hD45J12bY
-  3CbP2V68A6155Sn3ANioYyLwzstomdKCoB
-  3M79iMukPB4NRDCj3YWMMwbY5juvd7qNh7
-  33f9XE7g733xR3PTiJPQ4iveg2uFVqT3vM
-  31vwfzZWLxM4rJP953dX6ByrRJUGmfxrb5
-  39Yd9S7Qqjjz4GHniuHJB5J1eJGA8y45V2
-  3Pk1LJZrZPe2nNfEZTv345fHogg4s3M9Di
-)
-
-declare bip84=(
-  bc1qafhhn3n9cxavp92k5k4mnh7p73dgpaj5pdfwzz
-  bc1q0sxlwxhc3xqsq9s3u8nc49na76ecvhff3q508k
-  bc1q75rrufu3rpkdkas526cdrxfqw5ygma5s3g57m5
-  bc1q763djn4ad4uvzaapx0uju3y7cv0uv9kmwk3na5
-  bc1qw2m8svahap4q9zk2vxetjejhxau77dmvg24704
-  bc1q58rah8rms0w84r0aclgxde4uf7h7zzumhp98cj
-  bc1qnzz4fzxrsyh63gs4j05krk343my0nea7vzypt9
-  bc1qht2h5luy0rn2eswduxas7eyvlp8jtmz5nuug0t
-  bc1qkwrumv9nkv0egek3q7a9mhyrv7zrdzk0788r73
-  bc1qfujhm79xkx9xmd27anld6v2qp38vh7fuz5ck5d
-  bc1qygwcsmr4wrwef6hqcsamhdgv3nek4977nl39ry
-  bc1qfh35dzfe64l6m5t9gfxdxkxcnft36x2jy8uzpq
-  bc1qzg59yuzr8vthd5wagjccqvzcnrl7j63yzh5u7r
-  bc1qxskhzfd65mzcy55z47xlvatwv8gmhga9mld2a9
-  bc1qe2w3f5mv6wuwrrx4le0e7rxzzz5rs4y0r6e6d8
-  bc1qtduecpujtezhudg0574mxw9ufmzyqnz0xd5cfa
-  bc1qj4edcj6phrw58a8n4zjzptv4sd5yktkswswgfg
-  bc1qlux38s0rt7yrn4hvxyek6mt2hwcxzcz2nlnz5t
-  bc1qm9mavhu7pwx370cundzmt5gekef73uyjt0yej3
-  bc1qjd0tzgt5u6lxjhpnx7wxxhru0uwq8h5jtukxnf
+seed=67f93560761e20617de26e0cb84f7234aaf373ed2e66295c3d7397e6d7ebe882ea396d5d293808b0defd7edd2babd4c091ad942e6a9351e6d075a29d4df872af
+declare -A keys=(
+  [/44h/0h/0h/0/0]=1AZnveys2k5taGCCF743RtrWGwc58UMeq
+  [/44h/0h/0h/0/1]=1AMYJTJyV4o1hwNACJtfdXBW6BiD1f5FXb
+  [/44h/0h/0h/0/2]=1NPFFtSiFRatoeUf35rwYb8j8C1u7sVhGa
+  [/44h/0h/0h/0/3]=1L44VTYEzWesp8cxnXcPGbUzuwTYoSW9at
+  [/44h/0h/0h/0/4]=1FK85vpZavzZu6oBCvBcmD4FWXQT5fVYRu
+  [/44h/0h/0h/0/5]=12QaHfWLtyuMwNXuap3FscMY434bw4TS6n
+  [/44h/0h/0h/0/6]=1NeFG5BYAR9bnjAG72SDYKvNZBH4kPa8r1
+  [/44h/0h/0h/0/7]=1yF3BiHqbQKL4aRfNYHQt4ZpgNagC4nQe
+  [/44h/0h/0h/0/8]=144vmUhuAZJsV3m2GsP5Kqp55Pmzwx2gna
+  [/44h/0h/0h/0/9]=1DQM5w6C7gNaCKBxQV3rXKftcamRKDPQ2M
+  [/44h/0h/0h/0/10]=17XRvBac5xpgMVr6LbsDA56fgsaAed4oEV
+  [/44h/0h/0h/0/11]=1BSQC3Qn38UT2WVfcM6LdybkfE7tTGW5M2
+  [/44h/0h/0h/0/12]=1KUG4EDePnG97xQNXtuU9Xmp4sThqFvSoS
+  [/44h/0h/0h/0/13]=18sXnPcBnXBRFBYbqr85aKPPNpwT4f52a8
+  [/44h/0h/0h/0/14]=15S2gpAVvprN1GPE44oXCdtkA4L7yQtBkX
+  [/44h/0h/0h/0/15]=1FvC2STfbj7dcr2ApAPhagnSCP5Dmy79nH
+  [/44h/0h/0h/0/16]=15VZHWTEjnQuJSvUHzS7K6gmYjNv4A5cVJ
+  [/44h/0h/0h/0/17]=1N4S7Z43gb22PDCcpjHhX25cgDSLxegdWm
+  [/44h/0h/0h/0/18]=1MzS2BktGqokVM4kDuB6VavjLuib72W2je
+  [/44h/0h/0h/0/19]=1GDLeWJ4FcK2uiTFvLshtVcBArA7M9ECxq
+  [/49h/0h/0h/0/0]=3PfdXXVGY2HQUBvDfBsbF1HPYCbJzn2pCn
+  [/49h/0h/0h/0/1]=3Jr1Xg44oeqkeVnJSBRpnv5wEKcfq2yL3M
+  [/49h/0h/0h/0/2]=3B4TrV6UjRiiQC6tC2oqQV1YEXtzVZDGGX
+  [/49h/0h/0h/0/3]=3Qi4nGjJprauc3s5x1gX7DMQNkst5TmVs4
+  [/49h/0h/0h/0/4]=3MC2F24goHaSntZ7yP9fCbB9Y5c2hxGbgM
+  [/49h/0h/0h/0/5]=3LJ1hRawmLtFfMxqLjmuFKvh5ofsng7pnj
+  [/49h/0h/0h/0/6]=3G5EAX2BjRdsKMTYPHUAv8H3dg24w4GUHu
+  [/49h/0h/0h/0/7]=324QyBxZhyxsNi5TkTiQxGS2t99FzFaKHG
+  [/49h/0h/0h/0/8]=3MrmwaxN5zvdKcv6teUaowwhR8s92TmXbp
+  [/49h/0h/0h/0/9]=37oKLxbVrtWoh4BPwfV2ZAN8VHgXx71kUb
+  [/49h/0h/0h/0/10]=3JMeVbU9GvCGw8pyX4UDvhVxQDETyU2QfE
+  [/49h/0h/0h/0/11]=3BvmZCWDtACAdNM15JuH2KYZx4ZfCos4pV
+  [/49h/0h/0h/0/12]=3N3njcCVwPifybCE3kyXitt57HimRcYYRi
+  [/49h/0h/0h/0/13]=33G2FDnBQjLV1TmQMPoXAcaDjsDsdhJDTM
+  [/49h/0h/0h/0/14]=3FC5NQshZaqFzcNiHX3M5N6mT1FSFGRu7j
+  [/49h/0h/0h/0/15]=3DaXKNrwnvM1tcDqQA6fi4Lgnd97GdeviM
+  [/49h/0h/0h/0/16]=38G6QHTqqjwEzumpHq2CRcEmN8ZnMdxvqA
+  [/49h/0h/0h/0/17]=3HBx3PVJwBNm2VvnfCNkNwTP7dRxU2vQ8t
+  [/49h/0h/0h/0/18]=3CpeerCAFB4PJCiFcqoYUdRQb1JnBfydHh
+  [/49h/0h/0h/0/19]=34pes2A1tVYL1aWSymPqKMPdTXcHdTSG8g
+  [/84h/0h/0h/0/0]=bc1qacwy02va0hajhuge9xf5cl3mrm9hmj7jhpvc87
+  [/84h/0h/0h/0/1]=bc1qkphlwh6e5k0zpqpctpf88gz2xv5ukxcqvj2x45
+  [/84h/0h/0h/0/2]=bc1q4vj6ypwc3tzfeqr66cjkhdfqzk37f2x5fvw7k5
+  [/84h/0h/0h/0/3]=bc1q8w7sz3cml688p3ts0ae50sk4n7adn42w36pqld
+  [/84h/0h/0h/0/4]=bc1qxsnnzvpe7wllzfc8maqy4flh68l9llx084x3n9
+  [/84h/0h/0h/0/5]=bc1qm8y4x5p676cexxz4ypqdzq4p5pyp20g9paawla
+  [/84h/0h/0h/0/6]=bc1qfzrzfzg9kq8da6gc9n8fy4s26qhgmxz5tallrq
+  [/84h/0h/0h/0/7]=bc1qzscqf5rulv66aqns2tejlqmshuk3tz9lnpmm3x
+  [/84h/0h/0h/0/8]=bc1qmdqxv5qufde866gqyf7355rs8276kawt98v63a
+  [/84h/0h/0h/0/9]=bc1q6s6v8wr78fgyja8ds88sjm7jxayjzqxfhlnzx5
+  [/84h/0h/0h/0/10]=bc1q8ysdd6f9xe2lzdjmscg0g97a9x3hup2xkjfat8
+  [/84h/0h/0h/0/11]=bc1qhz63ht8xxwzccv2ja2stufg3zpxp6k5qlt5tws
+  [/84h/0h/0h/0/12]=bc1qvuw0q074q7sfn2r7zlak5auuuadj2kn59nsg0s
+  [/84h/0h/0h/0/13]=bc1qen0lyrmag0g4kgy2xulwfahfgjl803ha7cjs8y
+  [/84h/0h/0h/0/14]=bc1qu8hagwvfcecf64rycz9mutycphs677pfcpkxus
+  [/84h/0h/0h/0/15]=bc1q02tx6pa8zymn2fnnuflxxwd39tc5vaxpse0wq7
+  [/84h/0h/0h/0/16]=bc1quazvkkdhwus93vzhqk0zszxh03v74rr3pa2y8s
+  [/84h/0h/0h/0/17]=bc1qcfswpjtxdphx0gtwqpkhr5tzpnm8yfskfz2sh8
+  [/84h/0h/0h/0/18]=bc1qak5fdj3ns7g95j2akh4549kjjj9hycjz25xe4q
+  [/84h/0h/0h/0/19]=bc1qppmtqmppec2n45w5uhr3gked7xzqh8z07h7r2t
 )
 
 declare -i n=0
-for i in {0..19}
+for path in "${!keys[@]}"
 do
   ((n++))
-  path=/44h/0h/0h/0/$i
   xxd -p -r <<<"$seed" |
-  bip32 "$path/N" |
+  case "${path::4}" in
+    /49h) bip49 "$path/N" ;;
+    /84h) bip84 "$path/N" ;;
+    *)    bip32 "$path/N" ;;
+  esac |
   {
     read -r xpubkey
-    if [[ "$(bitcoinAddress "$xpubkey")" = ${bip44[i]} ]]
-    then echo "ok $n - $path: ${bip44[i]}"
-    else echo "not ok $n - $path: expected ${bip44[i]}, got $(bitcoinAddress "$xpubkey")"
-    fi
-  }
-done
-
-for i in {0..19}
-do
-  ((n++))
-  path=/49h/0h/0h/0/$i
-  xxd -p -r <<<"$seed" |
-  bip49 "$path/N" |
-  {
-    read -r ypubkey
-    if [[ "$(bitcoinAddress "$ypubkey")" = ${bip49[i]} ]]
-    then echo "ok $n - $path: ${bip49[i]}"
-    else echo "not ok $n - $path: expected ${bip49[i]}, got $(bitcoinAddress "$ypubkey")"
-    fi
-  }
-done
-
-for i in {0..19}
-do
-  ((n++))
-  path=/84h/0h/0h/0/$i
-  xxd -p -r <<<"$seed" |
-  bip84 "$path/N" |
-  {
-    read -r zpubkey
-    if [[ "$(bitcoinAddress "$zpubkey")" = ${bip84[i]} ]]
-    then echo "ok $n - $path: ${bip84[i]}"
-    else echo "not ok $n - $path: expected ${bip84[i]}, got $(bitcoinAddress "$zpubkey")"
+    if [[ "$(bitcoinAddress "$xpubkey")" = ${keys[$path]} ]]
+    then echo "ok $n - $path: ${keys[$path]}"
+    else echo "not ok $n - $path: expected ${keys[$path]}, got $(bitcoinAddress "$xpubkey")"
     fi
   }
 done
